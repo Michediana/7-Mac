836e06ffa6a78fdfa748e472675736773291d4b3
Michediana <31746040+Michediana@users.noreply.github.com>
2026-09-21 11:20:24 +0200

    Project Init

---
+++ 7-Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  7-Mac
+//
+//  Created by Michele Diana on 21/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 7-Mac/__MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  __MacApp.swift
+//  7-Mac
+//
+//  Created by Michele Diana on 21/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct __MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
